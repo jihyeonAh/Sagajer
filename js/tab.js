9373de3c17dd008e0b1d4eef76e2a916855defd7
jihyeonAh <159402358+jihyeonAh@users.noreply.github.com>
2024-02-22 12:30:26 +0900

    Add files via upload

--- js/tab.js
+++ js/tab.js
@@ -12,11 +12,31 @@ $(document).ready(function () {
         $(this).addClass("active"); //선택한 탭에만 active클래스 붙임
         $(".tab_content").hide(); //모든 contents들 숨김
 
-        var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+        var activeTab = $(this).find("span").attr("data"); //Find the href attribute value to identify the active tab + content
         /*.attr( attributeName ) -> 선택한 요소의 속성의 값을 가져옵니다.
         .find()는 어떤 요소의 하위 요소 중 특정 요소를 찾을 때 사용합니다.*/
         $(activeTab).fadeIn(); //Fade in the active ID content(active되어있는 칸의 id 값을 가진 부분이 나타남)
         return false;
     });
+
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (
+          location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+          location.hostname == this.hostname
+        ) {
+          var target = $(this.hash);
+          target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+          if (target.length) {
+            $("html, body").animate(
+              {
+                scrollTop: target.offset().top,
+              },
+              500
+            ); //움직이는 시간 조정
+            return false;
+          }
+        }
+      });
 
 });
